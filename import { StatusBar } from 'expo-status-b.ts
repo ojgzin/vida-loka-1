ef1f2f48461fdfa121a148853309e4b03a9a2e29
ojgzin <126284989+ojgzin@users.noreply.github.com>
2023-06-02 16:56:00 -0300

    Add files via upload

---
+++ import { StatusBar } from 'expo-status-b.ts
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  const [previsao, setPrevisao] = useState()
+
+  useEffect(() => {
+    buscaPrevisao()
+  }, [])
+
+
+  function buscaPrevisao() {
+    axios.get('https://api.open-meteo.com/v1/forecast?latitude=24.3&longitude=52&current_weather=true&hourly=temperature_2m')
+      .then(function (res) {
+        setPrevisao(res.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+  
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 100}}>{previsao?.current_weather?.temperature} ºC</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
